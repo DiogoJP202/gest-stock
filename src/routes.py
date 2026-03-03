8c69e4d88db40151ebc379babe2ae13965d93527
Diogojp202 <diogojp202@gmail.com>
2026-03-03 20:26:13 -0300

    Estado funcional

--- src/routes.py
+++ src/routes.py
@@ -2,6 +2,12 @@ from src.Application.Controllers.user_controller import UserController
 from flask import jsonify, make_response
 
 def init_routes(app):    
+    @app.route('/', methods=['GET'])
+    def index():
+        return make_response(jsonify({
+            "message": "Server is running",
+        }), 200)
+
     @app.route('/api', methods=['GET'])
     def health():
         return make_response(jsonify({
